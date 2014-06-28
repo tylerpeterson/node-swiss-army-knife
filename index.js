77daed9d6319d093e7f12927614ca48e2bfdd112
Tyler Peterson <petersontb@familysearch.org>
2014-06-27 20:01:56 -0600

    Pull function out of loop.

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ function allPermutations(choices, sequenceLength) {
   if (!(sequenceLength > 0)) {
     return [];
   }
-
+  
+  var i;
   var permutations = choices.slice();
 
-  while (permutations.length != Math.pow(choices.length, sequenceLength)) {
-    permutations = choices.reduce(function (resultList, currentReplacement) {
-      return resultList.concat(permutations.map(function (currentStem) {
-        return currentReplacement + currentStem;
-      }));
-    }, []);
+  function permute(resultList, currentReplacement) {
+    return resultList.concat(permutations.map(function (currentStem) {
+      return currentReplacement + currentStem;
+    }));
+  }
+
+  for (i = 1; i < sequenceLength; ++i) {
+    permutations = choices.reduce(permute, []);
   }
 
   return permutations;
